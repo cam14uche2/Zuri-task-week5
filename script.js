aa49bee58bcfc38814fb78b32298d824c7d5882e
Cam14uche2 <cam14uche@gmail.com>
2022-10-26 12:16:24 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,19 @@
+var fs = require('fs');
+var http =require('http');
+
+
+http.createServer((req, res) => {
+    fs.readFile('home.html', (err, data) => {
+        if (err) throw err;
+        res.writeHead(200, {
+            'Content-Type': "text/html"
+        });
+        res.write(data);
+        res.end();
+    })
+
+
+
+}).listen(4000);
+
+
